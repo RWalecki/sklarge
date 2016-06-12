3580b5e5af5b834388c35de0ccd10a1413595230
Robert Walecki <rw2614@imperial.ac.uk>
2016-06-13 00:40:09 +0100

    cleanup

--- demo.py
+++ demo.py
@@ -1,13 +1,40 @@
-from sklearn.datasets import make_multilabel_classification
 import multioutput_evaluation as me
+from sklearn import model_selection
 import numpy as np
+import os
+import h5py
 
-# generate multilabel dataset 
-X,y = make_multilabel_classification(n_classes=10,random_state=0)
+f = h5py.File('tests/data/test.h5')
 
-# 10 subjects
-S = np.random.randint(0,10,y.shape[0])
-
-# run X fold subject independent corss validation
+cv = model_selection.LabelKFold(10)
 clf = me.sk_estimator.MVR()
-me.run(clf, X, y, S, folds=3, out='/tmp')
+
+
+# simple fit/prediction
+clf.fit(f['X'],f['y'])
+y_hat = clf.predict(f['X'])
+
+# apply tr/te split
+clf.fit(f['X'],f['y'],np.arange(0,50))
+y_hat = clf.predict(f['X'],np.arange(50,100))
+
+# run the same but with numpy arrays as input data
+clf.fit(f['X'][:],f['y'][:])
+y_hat = clf.predict(f['X'][:])
+
+# apply parameter search
+clf = me.GridSearchCV(
+        clf,
+        clf.param_grid,
+        cv=cv,
+        n_jobs=-1,
+        verbose = 2,
+        output = '/tmp/oo',
+        mode = 'c'
+        )
+
+clf.fit(
+        X = '/vol/hmi/projects/robert/data/CNN_DATA/data_gray/fera2015.h5/points',
+        y = '/vol/hmi/projects/robert/data/CNN_DATA/data_gray/fera2015.h5/au_int',
+        labels = '/vol/hmi/projects/robert/data/CNN_DATA/data_gray/fera2015.h5/subject_id',
+        )
