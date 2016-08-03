202d3aa4284b8893f1812689fdd73f0e55499391
Robert Walecki <rw2614@imperial.ac.uk>
2016-08-04 00:24:12 +0100

    add demo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name="multioutput_evaluation",
+    name="sklarge",
     version="0.0.1",
     author="Robert Walecki",
     author_email="r.walecki14@imperial.ac.uk",
