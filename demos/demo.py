4a7312cbff9d109ac65190bb15e0de0060c18b1f
Robert Walecki <rw2614@imperial.ac.uk>
2016-08-04 15:55:35 +0100

    add demos

--- demos/demo.py
+++ demos/demo.py
@@ -1,19 +1,11 @@
-from sklearn.model_selection import GridSearchCV as GridSearchCV_old
 from sklearn.model_selection import LabelKFold
 from sklearn.linear_model import Ridge
-
 from sklarge.model_selection import GridSearchCV, Eval
-from sklarge.metrics import mse, pcc
-import os
 import h5py
 import numpy as np
 
-pwd = os.path.dirname(os.path.abspath(__file__))
 
-
-f = h5py.File(pwd+'/projects/sklarge/tests/data/test.h5')
-print(f['X'])
-print(f['y'])
+f = h5py.File('tests/data/test.h5')
 
 clf = Ridge()
 param_grid = {'alpha':10.**np.arange(-2,3)}
@@ -23,11 +15,12 @@ GS = GridSearchCV(
         param_grid = param_grid,
         cv = LabelKFold(2),
         n_jobs = -1,
+        out_path = '.tmp'
         )
+
 GS.fit(
         X = f['X'][::],
         y = f['y'][::],
-        tmp = pwd+'/tmp3/',
         submit='local',
         )
-Eval(pwd+'/tmp3/'+clf.__class__.__name__)
+Eval('.tmp')
