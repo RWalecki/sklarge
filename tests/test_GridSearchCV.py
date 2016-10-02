3dfa9532afbdcc74ffa1bb820b27e23ef8506412
Robert Walecki <rw2614@imperial.ac.uk>
2016-10-02 01:42:18 +0100

    add unit tests

--- tests/test_GridSearchCV.py
+++ tests/test_GridSearchCV.py
@@ -1,186 +1,66 @@
 import os
 import h5py 
 import numpy as np
+# import estimator
 
-from sklearn.model_selection import GridSearchCV as GridSearchCV_old
-from sklearn.model_selection import LabelKFold
-from sklearn.linear_model import Ridge
+from sklarge import GridSearchCV, run_local, evaluation
+from sklearn.linear_model import SGDRegressor 
+from sklearn.multioutput import MultiOutputClassifier 
 
-from sklarge.model_selection import GridSearchCV, Eval
 from sklarge.metrics import mse, pcc
-
 pwd = os.path.dirname(os.path.abspath(__file__))
 
 
-f = h5py.File(pwd+'/data/test.h5')
-clf = Ridge()
-param_grid = {'alpha':10.**np.arange(-2,3)}
+X = [pwd+'/data/X000000.h5', pwd+'/data/X000001.h5']
+y = [pwd+'/data/y000000.h5', pwd+'/data/y000001.h5']
+
+clf = MultiOutputClassifier(SGDRegressor())
+para = {'estimator__alpha': 10.**np.arange(-2,3)}
+
 
 class testcase:
 
-    def test_sk_estimator(self):
-        clf.fit(f['X'],f['y'])
-        y_hat = clf.predict(f['X'])
-        assert y_hat.shape == f['y'].shape
+    def test_cross_validation(self):
 
-    def test_multiple_run(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                n_jobs = -1,
-                out_path = 'tmp',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'][:,5],
-                )
-        Eval('tmp/')
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                n_jobs = -1,
-                out_path = 'tmp',
-                )
-
-        GS.fit(
-                X = f['X'],
-                y = f['y'][:,5],
-                )
-        Eval('tmp/')
-
-    def test_grid_search_basic(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                n_jobs = -1,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'][:,5],
-                )
-        Eval(pwd+'/tmp/')
-
-    def test_grid_search_basic_multi_output(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                n_jobs = -1,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'],
-                )
-        Eval(pwd+'/tmp/')
-
-    def test_grid_search_different_scoring(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                scoring=[pcc,mse],
-                n_jobs = -1,
-                verbose = 2,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'],
-                )
-        Eval(pwd+'/tmp/')
-
-    def test_grid_search_different_numpy_input(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                scoring=[pcc,mse],
-                n_jobs = -1,
-                verbose = 2,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'][::],
-                y = f['y'][::],
-                )
-        Eval(pwd+'/tmp/')
-        GS.fit(
-                X = f['X'],
-                y = f['y'][::],
-                )
-        Eval(pwd+'/tmp/')
-
-    def test_grid_search_labels(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                scoring=[pcc,mse],
-                n_jobs = -1,
-                verbose = 2,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'],
-                labels = np.arange(f['X'].shape[0]),
-                )
-        Eval(pwd+'/tmp/')
-
-    def test_compare_with_sk_gridseach(self):
-        GS_old = GridSearchCV_old(
-                estimator = clf,
-                param_grid = {'alpha':10.**np.arange(-5,5)},
-                scoring = mse,
-                cv = LabelKFold(2),
-                )
-
-        GS_new = GridSearchCV(
-                estimator = clf,
-                param_grid = {'alpha':10.**np.arange(-5,5)},
-                scoring = mse,
-                cv = LabelKFold(2),
-                )
-
-        GS_old.fit(f['X'][::],f['y'][::,5],f['S'][::])
-        GS_new.fit(f['X'][::],f['y'][::,5],f['S'][::])
-
-        assert GS_old.best_score_-GS_new.get_best_score() < 1e-4
-        assert GS_old.best_params_['alpha']==GS_new.get_best_param()['alpha']
-
-    def test_cn_folds(self):
-        GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(2),
-                cv_folds = [0],
-                n_jobs = -1,
-                out_path = pwd+'/tmp/',
-                )
-        GS.fit(
-                X = f['X'],
-                y = f['y'][:,5],
-                )
-        Eval(pwd+'/tmp/')
+        idx = [ [[0],[1]], [[1],[0]] ]
 
         GS = GridSearchCV(
-                estimator = clf,
-                param_grid = param_grid,
-                cv = LabelKFold(4),
-                cv_folds = [0,2],
-                n_jobs = -1,
-                out_path = pwd+'/tmp/',
+                estimator =  clf,
+                param_grid = para,
+                verbose=1
                 )
-        GS.fit(
-                X = f['X'],
-                y = f['y'][:,5],
+
+        GS._create_job_files(X,y,idx,pwd+'/tmp')
+        run_local(pwd+'/tmp',-1)
+        evaluation(pwd+'/tmp')
+
+    def test_tr_te_split(self):
+
+        idx = [ [ [0], [1] ] ]
+
+        GS = GridSearchCV(
+                estimator =  clf,
+                param_grid = para,
+                verbose=1
                 )
-        Eval(pwd+'/tmp/')
+
+        GS._create_job_files(X,y,idx,pwd+'/tmp')
+        run_local(pwd+'/tmp',-1)
+        evaluation(pwd+'/tmp')
+
+    def test_tr_tr(self):
+
+        idx = [ [ [0], [0] ] ]
+
+        GS = GridSearchCV(
+                estimator =  clf,
+                param_grid = para,
+                verbose=1
+                )
+
+        GS._create_job_files(X,y,idx,pwd+'/tmp')
+        run_local(pwd+'/tmp',-1)
+        evaluation(pwd+'/tmp')
 
 
 if __name__ == "__main__":
